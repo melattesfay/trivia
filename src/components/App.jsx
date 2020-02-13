f0db3f9e6a1863045a828b5cce1b02dfd8c234d1
Nataly Olivares <natis@Natalys-MacBook-Pro.local>
2020-02-12 17:21:46 -0800

    New changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Answer from './Answer';
+import {buildFirebase} from '../clients/firebase';
 // import components
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1 id="title">... Quiz!</h1>
+        <h1 id="title">Guess where this movie/tv show character is from! Quiz!</h1>
         <strong id="timer"> Timer: 0 </strong>
         <h1 class= "questionBox" id="q1"> Question 1:
         .......?</h1>
@@ -17,12 +18,7 @@ class App extends Component {
         <button class="box1"> one</button>
         <button class="box1"> one</button>
         <button class="box1"> one</button>
-        Trivia!
-        <Answer> </Answer>
-        <Answer> </Answer>
-        <Answer> </Answer>
-        <Answer> </Answer>
-
+       
         
       </div>
     );
