46f4a681bae5f0afa727ad89d6b153df0a78f8c1
Nataly Olivares <natis@Natalys-MacBook-Pro.local>
2020-02-26 17:36:57 -0800

    new changes to html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        currentQuestion: 1   
+        currentQuestion: 0   
     }
+  //   this.state = {
+  //     choices: 2   
+  // }
   }
+ nextBtn(){
+    var newCQ = this.state.currentQuestion + 1;
+    this.setState({ currentQuestion: newCQ });
+ }
+
+//  choicebox(){
+//   var newCB = this.state.choices + 1;
+//   this.setState({ choices: newCB });
+
+//  }
 
   
 
@@ -18,14 +31,15 @@ class App extends Component {
     return (
       <div className="app">
         <h1 id="title">Guess where this movie/tv show character is from! Quiz!</h1>
-        <strong id="timer"> Timer: 0 </strong>
         <h1 class= "questionBox" id="q1"> Question 1:
         {this.props.questionList[this.state.currentQuestion].question_text}</h1>
         <button class="choiceBox"> {this.props.questionList[0].choices[0]}</button>
-        <button class="choiceBox">  {this.props.questionList[0].choices[1]}</button>
-        <button class="choiceBox">  {this.props.questionList[0].choices[2]}</button>
-        <button class="choiceBox">  {this.props.questionList[0].choices[3]}</button>
+        <button class="choiceBox">  {this.props.questionList[0].choices[0]}</button>
+        <button class="choiceBox">  {this.props.questionList[0].choices[0]}</button>
+        <button class="choiceBox">  {this.props.questionList[0].choices[0]}</button>
         
+        <button onClick={() => this.nextBtn()}>Next Question</button>
+
        
         
       </div>
